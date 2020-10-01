fd75fb8ca567d239a2abf800879eaf5ef4798eed
carloshenche <carloshenche@metricool.com>
2020-10-01 08:36:57 +0200

    Setup sha256 as encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.naming.ldap.LdapContext;
@@ -42,22 +43,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new LdapShaPasswordEncoder();
+        return new StandardPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("spring")
-                .password("{SSHA}IRyPgDThpZ3O3YCTYy9R/4NsORseszGF8fDADg==") //To setup password encoder use {}, noop is none
+                .password("7d5d00c7cf1f8ef2e364ec56577f0a682060e8121e6d92eac4f0443e8483a21b7509980ab6822bd4") //To setup password encoder use {}, noop is none
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{SSHA}eT9sLhDBWzQGuk0hhZh/FYVGok6aXuDT5alZrw==") //To setup password encoder use {}, noop is none
+                .password("e8da526717178a3f15a01deea27b2a71266e1bf85925f67373ceb78f9b35033c57a3588a42d3bb34") //To setup password encoder use {}, noop is none
                 .roles("USER")
                 .and()
                 .withUser("scot")
-                .password("{SSHA}ccR4lrMTSgbPPXQoL/VpnjtFWgc9zivSwczhJw==") //To setup password encoder use {}, noop is none
+                .password("77c831d237553a13e22a9970f81e756ee239aac3d7e64197b9d4530bd1a5954863b597958975cbd3") //To setup password encoder use {}, noop is none
                 .roles("CUSTOMER");
     }
 
--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTests.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTests.java
@@ -5,7 +5,10 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,6 +16,14 @@ public class PasswordEncodingTests {
 
     static final String PASSWORD = "password";
 
+    @Test
+    void testSha256() {
+        PasswordEncoder sha256 = new StandardPasswordEncoder();
+        System.out.println(sha256.encode(PASSWORD));
+        System.out.println(sha256.encode(PASSWORD));
+        System.out.println("guru->" + sha256.encode("guru"));
+        System.out.println("tiger->"+sha256.encode("tiger"));
+    }
 
     @Test
     void testLdap() {
