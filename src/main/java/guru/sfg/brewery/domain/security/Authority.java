eb7600f8af1e91335369d04fae3154ebac645fa2
carloshenche <carloshenche@metricool.com>
2020-10-13 08:32:39 +0200

    Added lombok anotations

--- src/main/java/guru/sfg/brewery/domain/security/Authority.java
+++ src/main/java/guru/sfg/brewery/domain/security/Authority.java
@@ -1,11 +1,23 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 public class Authority {
