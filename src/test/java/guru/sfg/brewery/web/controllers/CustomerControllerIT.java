fde55573a0675b009dcbe0f77debf315ae257068
carloshenche <carloshenche@metricool.com>
2020-10-27 08:27:36 +0100

    Securized customer creation for admin role using preAuthorize annotation

--- src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
@@ -1,35 +1,75 @@
 package guru.sfg.brewery.web.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class CustomerControllerIT extends BaseIT{
 
-    @ParameterizedTest(name = "#{index} with [{arguments}]")
-    @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamAdminCustomer")
-    public void testListCustomersAUTH(String user,String pwd) throws Exception {
-        mockMvc.perform(get("/customers")
-                .with(httpBasic(user,pwd)))
-                .andExpect(status().isOk());
+    @DisplayName("List customers")
+    @Nested
+    class ListCustomers {
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamAdminCustomer")
+        public void testListCustomersAUTH(String user, String pwd) throws Exception {
+            mockMvc.perform(get("/customers")
+                    .with(httpBasic(user, pwd)))
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        public void testListCustomersNOAUTH() throws Exception {
+            mockMvc.perform(get("/customers")
+                    .with(httpBasic("user", "password")))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void testListCustomersNOLOGGEDIN() throws Exception {
+            mockMvc.perform(get("/customers"))
+                    .andExpect(status().isUnauthorized());
+        }
     }
 
-    @Test
-    public void testListCustomersNOAUTH() throws Exception {
-        mockMvc.perform(get("/customers")
-                .with(httpBasic("user","password")))
-                .andExpect(status().isForbidden());
-    }
+    @DisplayName("Add customers")
+    @Nested
+    class AddCustomers{
 
-    @Test
-    public void testListCustomersNOLOGGEDIN() throws Exception {
-        mockMvc.perform(get("/customers"))
-                .andExpect(status().isUnauthorized());
+        @Rollback
+        @Test
+        public void processCreationForm() throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo customer")
+                    .with(httpBasic("spring","guru")))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamNotAdmin")
+        @Rollback
+        public void processCreationFormNOAUTH(String user,String pwd) throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo customer")
+                    .with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void processCreationFormNOAUTH() throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo customer"))
+                    .andExpect(status().isUnauthorized());
+        }
+
     }
 }
