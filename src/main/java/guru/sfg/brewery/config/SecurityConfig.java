b8adcd89b52ff0b6947bdbb52a5ed030c253be01
carloshenche <carloshenche@metricool.com>
2020-11-30 08:38:34 +0100

    Add remember me by cookie

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import guru.sfg.brewery.security.RestHeaderAuthFilter;
 import guru.sfg.brewery.security.RestUrlAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,15 +12,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityEvaluationContextExtension securityEvaluationContextExtension(){
@@ -83,7 +88,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 .logoutSuccessUrl("/?logout").permitAll();
                     })
                 .httpBasic()
-                .and().csrf().ignoringAntMatchers("/h2-console/**","/api/**");
+                .and().csrf().ignoringAntMatchers("/h2-console/**","/api/**")
+                .and().rememberMe().key("sfg-key").userDetailsService(userDetailsService);
 
         http.headers().frameOptions().sameOrigin();
     }
