ed59d0490b51e3e4d58035638ca25eac9ff1ba9e
carloshenche <carloshenche@metricool.com>
2020-12-14 08:39:32 +0100

    Save failure events

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,5 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.repositories.security.LoginFailureRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,23 +11,33 @@ import org.springframework.security.authentication.event.AuthenticationFailureBa
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 @Slf4j
 public class AuthenticationFailureListener {
+
+    private final LoginFailureRepository loginFailureRepository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("User bad cred");
         if(event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken)event.getSource();
             if(token.getPrincipal()instanceof String){
                 String userName = (String)token.getPrincipal();
-                log.debug("Username logged in: " +userName);
+                builder.username(userName);
+                log.debug("Attempted username: " +userName);
+                userRepository.findByUsername(userName).ifPresent(builder::user);
             }
             if(token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails)token.getDetails();
                 log.debug("Source IP: " + details.getRemoteAddress());
+                builder.sourceIp(details.getRemoteAddress());
             }
+            LoginFailure failure = loginFailureRepository.save(builder.build());
+            log.debug("Failure event: " + failure.getId());
         }
     }
 }
