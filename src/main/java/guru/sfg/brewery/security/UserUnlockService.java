7f2abb1f69a2dc580b3984e7a9e0d6c909b96c8f
carloshenche <carloshenche@metricool.com>
2020-12-17 08:53:03 +0100

    Unlocking accounts by scheduled task

---
+++ src/main/java/guru/sfg/brewery/security/UserUnlockService.java
@@ -0,0 +1,34 @@
+package guru.sfg.brewery.security;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserUnlockService {
+
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
+    public void unlockAccounts() {
+        log.debug("Running unlock accounts");
+
+        List<User> lockedUsers = this.userRepository.findAllByAccountNonLockedAndLastModifiedDateIsBefore(false,
+                Timestamp.valueOf(LocalDateTime.now().minusSeconds(30)));
+
+        if(!lockedUsers.isEmpty()) {
+            log.debug("Locked accounts found. Unlocking ...");
+            lockedUsers.forEach(user -> {user.setAccountNonLocked(true);});
+            this.userRepository.saveAll(lockedUsers);
+        }
+    }
+}
