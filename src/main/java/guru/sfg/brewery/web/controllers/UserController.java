6a3c46bb599f48eeef4cc9cf8a46f9958ee563a8
carloshenche <carloshenche@metricool.com>
2021-01-18 08:35:23 +0100

    Verify 2FA code

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -36,13 +36,22 @@ public class UserController {
         return "user/register2fa";
     }
 
-    private User getUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
     @PostMapping("register2fa")
     public String confirm2fa(@RequestParam Integer verifyCode){
-        //TODO - impl
-        return "index";
+        User user = getUser();
+        log.debug("Verification code is: {}", verifyCode);
+
+        if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)){
+            User savedUser = this.userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUserGoogle2fa(true);
+            this.userRepository.save(savedUser);
+            return "index";
+        } else {
+            return "user/register2fa";
+        }
+    }
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
