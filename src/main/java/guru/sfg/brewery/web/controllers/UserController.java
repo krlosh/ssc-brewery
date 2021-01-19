674c6bba82634acdc36e194ec0eb9c85811747e4
carloshenche <carloshenche@metricool.com>
2021-01-19 08:20:25 +0100

    Verify 2fa UI

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -54,4 +54,21 @@ public class UserController {
     private User getUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
+
+    @GetMapping("/verify2fa")
+    public String verify2fa(){
+        return "user/verify2fa";
+    }
+
+    @PostMapping("/verify2fa")
+    public String verifyPostOf2fa(@RequestParam Integer verifyCode){
+        User user = getUser();
+
+        if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)){
+            ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setGoogle2faRequired(false);
+            return "index";
+        } else {
+            return "user/verify2fa";
+        }
+    }
 }
